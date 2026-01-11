18c77fcbd88ac9761e46374b4b894288b3299de7
cosmiinnn <cosmin.chiruta02@gmail.com>
2026-01-11 15:16:03 +0200

    secrets, dont tell anyone

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5R1ZHGaDBSQD7Qq8oL8ERIVMh3DHI-XQ",
-  authDomain: "recipe-platform-dsw.firebaseapp.com",
-  projectId: "recipe-platform-dsw",
-  storageBucket: "recipe-platform-dsw.firebasestorage.app",
-  messagingSenderId: "552250922695",
-  appId: "1:552250922695:web:fd22d7196c1ad31ab450e2",
-  measurementId: "G-N43FG9FMC2"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
